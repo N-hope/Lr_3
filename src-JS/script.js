b7eda5b1c1685f8c399b026520343039e1719f3f
N-hope <nadiaK3009@gmail.com>
2022-11-28 02:34:40 +0200

    #1(8)

---
+++ src-JS/script.js
@@ -0,0 +1,29 @@
+function T(){
+  var now = new Date();
+  var pib = "";
+  var table = document.body.children[2];
+    for(var i=1; i<6; i++){
+		var people = table.rows[i].cells[0].innerHTML;
+		pib = people;
+		
+		var DateHuman = new Date(table.rows[i].cells[1].innerHTML);
+		var y = parseInt(((now-DateHuman)/1000/3600/24/365));
+		var D = new Date(table.rows[i].cells[2].innerHTML = y + " років");
+		console.log(pib +" "+ y);
+	}
+}
+//<script src="script.js" defer></script>
+
+/*
+//↓працює
+document.getElementById("dtTbl").onclick=function(){
+var table = document.body.children[3];
+for(var i=1; i<6; i++){
+	a= alert(table.rows[i].cells[1].innerHTML);
+	}
+}
+var now = new Date();
+var d1 = new Date(2007, 8, 27);
+	var y = ((now-d1)/3600/24/1000);
+	alert( y +"days or " +(y/365)+ " years");
+*/
